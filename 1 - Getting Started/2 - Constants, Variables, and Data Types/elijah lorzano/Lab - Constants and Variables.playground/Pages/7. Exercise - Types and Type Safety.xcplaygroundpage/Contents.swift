a05fabb870f3104f4c2be3bbb911bf9377463dfd
ejpl1 <ejplorzano@gmail.com>
2021-07-08 11:11:25 -0500

    Initial commit

---
+++ 1 - Getting Started/2 - Constants, Variables, and Data Types/elijah lorzano/Lab - Constants and Variables.playground/Pages/7. Exercise - Types and Type Safety.xcplaygroundpage/Contents.swift
@@ -0,0 +1,23 @@
+/*:
+ ## Exercise - Types and Type Safety
+ 
+ Declare two variables, one called `firstDecimal` and one called `secondDecimal`. Both should have decimal values. Look at both of their types by holding Option and clicking on the variable name.
+ */
+var firstDecimal = 0.01
+var secondDecimal = 0.02
+/*:
+ Declare a variable called `trueOrFalse` and give it a boolean value. Try to assign it to `firstDecimal` like so: `firstDecimal = trueOrFalse`. Does it compile? Print a statement to the console explaining why not, and remove the line of code that will not compile.
+ */
+var trueOrFalse = true
+print("You cannot assign a boolean value to doubles.")
+/*:
+ Declare a variable and give it a string value. Then try to assign it to `firstDecimal`. Does it compile? Print a statement to the console explaining why not, and remove the line of code that will not compile.
+ */
+var gamer = "a string value"
+print("You cannot assign a string value to doubles")
+/*:
+ Finally, declare a variable with a whole number value. Then try to assign it to `firstDecimal`. Why won't this compile even though both variables are numbers? Print a statement to the console explaining why not, and remove the line of code that will not compile.
+ */
+var wholeNumber = 1
+print("one is an integer value while the other is a double value, which can hold whole number values but only while represented as a decimal such as '1.0.'")
+//: [Previous](@previous)  |  page 7 of 10  |  [Next: App Exercise - Tracking Different Types](@next)
